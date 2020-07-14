b686f80513ae155671da63b138c57818aadda6aa
miphc42 <philhc02@gmail.com>
2020-07-14 16:00:26 -0400

    node and mongodb

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,17 @@
-// document.querySelector('.signup').addEventListener('click', function(){
-//     window.open("https://www.google.com");
+// var MongoClient = require('mongodb').MongoClient;
+// var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     var myobj = { name: "Company Inc", address: "Highway 37" };
+//     dbo.collection("customers").insertOne(myobj, function(err, res) {
+//         if (err) throw err;
+//         console.log("1 document inserted");
+//     });
+//     dbo.collection("customers").findOne({}, function(err, result) {
+//         if (err) throw err;
+//         console.log(result.name);
+//         db.close();
+//     });
 // });
